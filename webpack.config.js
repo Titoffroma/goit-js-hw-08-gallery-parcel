da50b5f6eadd7a63393941781ada2e15c75e5756
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2020-10-17 23:54:14 +0300

    web-pack-config added

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+};
